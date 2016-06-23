29d8ab3574781bffcc08cf17e6dd440da7238073
Ralph Schmieder <rschmied@cisco.com>
2016-06-23 10:18:18 +0200

    consistency

--- sample2.py
+++ sample2.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
-import requests
+import requests, json
 
 requests.packages.urllib3.disable_warnings()
 
 url = 'https://apic:8888/api/aaaLogin.json'
 
-# this is a JSON string:
-payload = '{ "aaaUser": {"attributes": {"pwd": "1vtG@lw@y", "name": "admin" }}}'
 
-# this is a Python dictionary object:
+# these are Python dictionary objects:
+payload = {
+    "aaaUser": {
+        "attributes": {
+            "pwd": "1vtG@lw@y",
+            "name": "admin"
+        }
+    }
+}
 headers = { 'content-type': "application/json" }
 
 """ note the different quotes
@@ -19,7 +25,7 @@ headers = { 'content-type': "application/json" }
 '{"aaaUser": {"attributes": {"pwd": "1vtG@lw@y", "name": "admin"}}}'
 """
 
-response = requests.post(url, data=payload, headers=headers)
+response = requests.post(url, data=json.dumps(payload), headers=headers)
 token = response.cookies.get('APIC-cookie')
 
 url = 'https://apic:8888/api/node/class/dhcpClient.json'
